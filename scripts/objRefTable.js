1bdbb4fee61c3c4da2dcec07f9f9047883f6c328
onegameonedestiny <153502552+onegameonedestiny@users.noreply.github.com>
2025-11-01 17:52:09 +0800

    Add files via upload

--- scripts/objRefTable.js
+++ scripts/objRefTable.js
@@ -2,18 +2,29 @@ const C3 = self.C3;
 self.C3_GetObjectRefTable = function () {
 	return [
 		C3.Plugins.Button,
-		C3.Plugins.System.Cnds.OnLayoutStart,
+		C3.Plugins.TextBox,
 		C3.Plugins.Button.Cnds.OnClicked,
-		C3.JavaScriptInEvents.事件表1_Event4,
+		C3.JavaScriptInEvents.事件表1_Event2,
 		C3.Plugins.System.Acts.Wait,
-		C3.Plugins.Button.Acts.SetText
+		C3.Plugins.TextBox.Acts.SetText,
+		C3.Plugins.System.Acts.SetVar,
+		C3.Plugins.TextBox.Exps.Text,
+		C3.JavaScriptInEvents.事件表1_Event5,
+		C3.Plugins.Button.Acts.SetText,
+		C3.JavaScriptInEvents.事件表2_Event2
 	];
 };
 self.C3_JsPropNameTable = [
 	{按鈕: 0},
+	{按鈕2: 0},
+	{輸入框: 0},
+	{playerScore: 0},
+	{playTime: 0},
 	{NAME: 0}
 ];
 
 self.InstanceType = {
-	按鈕: class extends self.IButtonInstance {}
+	按鈕: class extends self.IButtonInstance {},
+	按鈕2: class extends self.IButtonInstance {},
+	輸入框: class extends self.ITextInputInstance {}
 }
--- scripts/project/javaScriptInEvents.js
+++ scripts/project/javaScriptInEvents.js
@@ -2,11 +2,53 @@
 
 const scriptsInEvents = {
 
-	async 事件表1_Event4(runtime, localVars)
+	async 事件表1_Event2(runtime, localVars)
 	{
-		const n = localStorage.getItem("line_name");
-		alert("line_name = " + n);
-		runtime.globalVars.NAME = n;
+		(async () => {
+		  const data = await liff.storage.get("gameData");
+		  if (data) {
+		    runtime.globalVars.playerScore = data.score || 0;
+		    runtime.globalVars.playTime = data.time || 0;
+		    console.log("✅ 已載入雲端資料:", data);
+		  } else {
+		    console.log("⚠️ 沒有找到任何雲端存檔");
+		  }
+		})();
+		
+	},
+
+	async 事件表1_Event5(runtime, localVars)
+	{
+		(async () => {
+		  const data = {
+		    score: runtime.globalVars.playerScore,
+		    time: runtime.globalVars.playTime,
+		  };
+		
+		  await liff.storage.set("gameData", data);
+		  console.log("✅ 已將資料上傳到 liff.storage");
+		})();
+		
+	},
+
+	async 事件表2_Event2(runtime, localVars)
+	{
+		(async () => {
+		  const data = await liff.storage.get("gameData");
+		  if (data) {
+		    runtime.globalVars.playerScore = data.score || 0;
+		    runtime.globalVars.playTime = data.time || 0;
+		    console.log("✅ 已載入雲端資料:", data);
+		  } else {
+		    console.log("⚠️ 沒有找到任何雲端存檔");
+		  }
+		})();
+		
+	},
+
+	async 事件表2_Event3(runtime, localVars)
+	{
+
 	}
 };
 
